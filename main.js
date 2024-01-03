3ead62559de57b998c94d0899128bc8dddbe9f52
Aditstr <adettsatrio@gmail.com>
2024-01-03 18:28:18 +0700

    hapus beberapa js

--- main.js
+++ main.js
@@ -12,17 +12,3 @@ function filterData(search) {
     }
   });
 }
-
-const bookImages = document.querySelectorAll(".card");
-
-bookImages.forEach((image) => {
-  image.addEventListener("mouseover", () => {
-    image.style.transform = "scale(1.1)";
-    image.style.transition = "transform 0.3s ease";
-  });
-
-  image.addEventListener("mouseout", () => {
-    image.style.transform = "scale(1)";
-    image.style.transition = "transform 0.3s ease";
-  });
-});
